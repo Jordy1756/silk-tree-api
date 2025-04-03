3bd4c6734ac62b70ee181b4a23e0c303e191d030
Jordy756 <jordycastro1756@gmail.com>
2025-04-03 14:58:34 -0600

    Agregar verificación de citas médicas superpuestas en el repositorio. Implementar método para comprobar solapamientos antes de insertar o actualizar citas.

--- src/features/medical-appointments/domain/interfaces/IMedicalAppointmentRepository.ts
+++ src/features/medical-appointments/domain/interfaces/IMedicalAppointmentRepository.ts
@@ -4,5 +4,10 @@ export interface IMedicalAppointmentRepository {
     insertMedicalAppointment(medicalAppointment: MedicalAppointment): Promise<MedicalAppointment>;
     updateMedicalAppointment(medicalAppointment: MedicalAppointment): Promise<Number>;
     deleteMedicalAppointment(medicalAppointmentId: string): Promise<number>;
+    checkOverlappingMedicalAppointment(
+        specialtyId: number,
+        startDate: Date,
+        endDate: Date
+    ): Promise<MedicalAppointment | null>;
     getAllMedicalAppointments(): Promise<MedicalAppointment[]>;
 }
--- src/features/medical-appointments/infrastructure/repositories/MedicalAppointmentRepository.ts
+++ src/features/medical-appointments/infrastructure/repositories/MedicalAppointmentRepository.ts
@@ -2,6 +2,7 @@ import { injectable } from "inversify";
 import { MedicalAppointment } from "../../domain/entities/MedicalAppointment";
 import { IMedicalAppointmentRepository } from "../../domain/interfaces/IMedicalAppointmentRepository";
 import { Specialty } from "../../domain/entities/Specialty";
+import { Op } from "sequelize";
 
 @injectable()
 export class MedicalAppointmentRepository implements IMedicalAppointmentRepository {
@@ -11,6 +12,9 @@ export class MedicalAppointmentRepository implements IMedicalAppointmentReposito
         endDate,
         specialtyId,
     }: MedicalAppointment): Promise<MedicalAppointment> {
+        if (await this.checkOverlappingMedicalAppointment(specialtyId, startDate, endDate))
+            throw new Error("Horario no disponible");
+
         return await MedicalAppointment.create({ title, startDate, endDate, specialtyId });
     }
 
@@ -21,11 +25,26 @@ export class MedicalAppointmentRepository implements IMedicalAppointmentReposito
         endDate,
         specialtyId,
     }: MedicalAppointment): Promise<Number> {
+        if (await this.checkOverlappingMedicalAppointment(specialtyId, startDate, endDate))
+            throw new Error("Horario no disponible");
+
         return (await MedicalAppointment.update({ title, startDate, endDate, specialtyId }, { where: { id } }))[0];
     }
 
     async deleteMedicalAppointment(medicalAppointmentId: string): Promise<number> {
         return await MedicalAppointment.destroy({ where: { id: medicalAppointmentId } });
+    }
+
+    async checkOverlappingMedicalAppointment(specialtyId: number, startDate: Date, endDate: Date) {
+        return await MedicalAppointment.findOne({
+            where: {
+                specialtyId,
+                [Op.or]: [
+                    { startDate: { [Op.between]: [startDate, endDate] } },
+                    { endDate: { [Op.between]: [startDate, endDate] } },
+                ],
+            },
+        });
     }
 
     async getAllMedicalAppointments(): Promise<MedicalAppointment[]> {
