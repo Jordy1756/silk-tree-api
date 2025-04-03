a2d27abbf88a3abbb6cb10caabada877a615669d
Jordy756 <jordycastro1756@gmail.com>
2025-04-03 14:58:47 -0600

    Eliminar método de verificación de citas médicas superpuestas del repositorio.

--- src/features/medical-appointments/domain/interfaces/IMedicalAppointmentRepository.ts
+++ src/features/medical-appointments/domain/interfaces/IMedicalAppointmentRepository.ts
@@ -4,10 +4,5 @@ export interface IMedicalAppointmentRepository {
     insertMedicalAppointment(medicalAppointment: MedicalAppointment): Promise<MedicalAppointment>;
     updateMedicalAppointment(medicalAppointment: MedicalAppointment): Promise<Number>;
     deleteMedicalAppointment(medicalAppointmentId: string): Promise<number>;
-    checkOverlappingMedicalAppointment(
-        specialtyId: number,
-        startDate: Date,
-        endDate: Date
-    ): Promise<MedicalAppointment | null>;
     getAllMedicalAppointments(): Promise<MedicalAppointment[]>;
 }
