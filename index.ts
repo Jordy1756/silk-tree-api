0c5739db28f8e20427312551a277072c8370c3cf
Jordy756 <jordycastro1756@gmail.com>
2025-04-21 23:05:58 -0600

    Refactor index.ts: reorder imports, remove unnecessary lines, and add sequelize sync before export; add new vercel.json configuration for deployment

--- index.ts
+++ index.ts
@@ -4,11 +4,10 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { InversifyExpressServer } from "inversify-express-utils";
 import { Container } from "inversify";
-import { PORT } from "./src/shared/config/environment";
 import { configureUserContainer } from "./src/features/users/infrastructure/container/UserContainer";
-import { sequelize } from "./src/shared/database/connection";
 import { configureMedicalAppointmentContainer } from "./src/features/medical-appointments/infrastructure/container/MedicalAppointmentContainer";
 import { errorMiddleware } from "./src/shared/middlewares/errorMiddleware";
+import { sequelize } from "./src/shared/database/connection";
 
 const app = express();
 app.disable("x-powered-by");
@@ -17,7 +16,6 @@ app.use(json());
 app.use(cookieParser());
 
 const rootContainer = new Container();
-
 configureUserContainer(rootContainer);
 configureMedicalAppointmentContainer(rootContainer);
 
@@ -25,6 +23,10 @@ const server = new InversifyExpressServer(rootContainer, null, { rootPath: "/api
 const appConfigured = server.build();
 
 appConfigured.use(errorMiddleware);
-appConfigured.listen(PORT || 5000, () => console.log(`Running on http://localhost:${PORT || 5000}/api`));
 
+// Sin appConfigured.listen()
+
+// Sincroniza la base de datos antes de exportar (puedes manejarlo mejor según tu lógica)
 (async () => await sequelize.sync())();
+
+export default appConfigured;
